d9aded84f527407815fcfddbfd6c6389a2c5195f
Khorchin Erdemt <paowenpo@126.com>
2026-08-30 08:34:12 +0800

    init

--- bin/train-models.sh
+++ bin/train-models.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-yolo train project=yolo name=exp model=$0 data=datasets/$1/configs.yaml \
+yolo train project=$1 name=$0 model=$0 data=datasets/$1/configs.yaml \
   epochs=200 batch=32 imgsz=640 device=0 workers=4 patience=20 augment=True
 
-yolo val model=yolo/exp/weights/best.pt data=datasets/$1/configs.yaml split=test
+yolo val model=$1/$0/weights/best.pt data=datasets/$1/configs.yaml split=test
 
-yolo export model=yolo/exp/weights/best.pt format=onnx
+yolo export model=$1/$1/weights/best.pt format=onnx
