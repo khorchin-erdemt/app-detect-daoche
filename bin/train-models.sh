4ba5c9ea3769e17b7ce9449d79a6a66b91ca51d8
Khorchin Erdemt <paowenpo@126.com>
2026-08-30 09:11:20 +0800

    init

--- bin/train-models.sh
+++ bin/train-models.sh
@@ -18,6 +18,6 @@ done
 yolo train project=$d name=$m model=$m.pt data=datasets/$d/configs.yaml \
     epochs=200 batch=32 imgsz=640 device=0 workers=4 patience=20 augment=True
 
-yolo val model=$d/$m/weights/best.pt data=datasets/$d/configs.yaml split=test
+yolo val model=runs/detect/$d/$m/weights/best.pt data=datasets/$d/configs.yaml split=test
 
-yolo export model=$d/$m/weights/best.pt format=onnx
+yolo export model=runs/detect/$d/$m/weights/best.pt format=onnx
