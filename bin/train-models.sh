e3a4f0f21078caf8efe72b9ddb1070631e6bfbc1
Khorchin Erdemt <paowenpo@126.com>
2026-08-30 08:57:54 +0800

    init

--- bin/train-models.sh
+++ bin/train-models.sh
@@ -2,9 +2,19 @@
 
 set -e
 
-yolo train project=$2 name=$1 model=$1.pt data=datasets/$2/configs.yaml \
-  epochs=200 batch=32 imgsz=640 device=0 workers=4 patience=20 augment=True
+m=""
+d=""
 
-yolo val model=$2/$1/weights/best.pt data=datasets/$2/configs.yaml split=test
+while getopts ":m:d" opt; do
+    case $opt in
+        m) m="$OPTARG" ;;
+        d) d="$OPTARG" ;;
+    esac
+done
 
-yolo export model=$2/$1/weights/best.pt format=onnx
+yolo train project=$d name=$m model=$m.pt data=datasets/$d/configs.yaml \
+    epochs=200 batch=32 imgsz=640 device=0 workers=4 patience=20 augment=True
+
+yolo val model=$d/$m/weights/best.pt data=datasets/$d/configs.yaml split=test
+
+yolo export model=$d/$m/weights/best.pt format=onnx
